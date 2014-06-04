6355c7e78760f773237e7d3562692786ec51cf82
Alex Pendleton <alexcpendleton@gmail.com>
2014-06-04 14:08:49 -0400

    Hey it works!
    Going to refactor some stuff for broader reusability

--- Chrome/src/inject/inject.js
+++ Chrome/src/inject/inject.js
@@ -1,4 +1,21 @@
-console.log("yo, content script loaded");
-console.log(".window", window, window.$);
-
-console.log(".document",document);
+(function() {
+    function isGist() {
+       return window.location.toString().toLowerCase().indexOf("https://gist.github") > -1;
+    }
+    function createConfirmationMessage() {
+        var messageName = isGist() ? "ConfirmGist" : "ConfirmProject";
+        return chrome.i18n.getMessage(messageName);
+    }
+	var forkButton = document.querySelector(".fork-button");
+    function onFork(event) {
+        var result = confirm(createConfirmationMessage());
+        if(!result) {
+            event.preventDefault();
+            event.stopPropagation();
+            event.stopImmediatePropagation();
+            return false;
+        }
+        return true;
+    }
+    forkButton.addEventListener("click", onFork, false);
+})();
