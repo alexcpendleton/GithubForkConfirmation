4ce650d54646c7708a6315aee964692c3469757e
Alex Pendleton <alexcpendleton@gmail.com>
2014-06-04 16:09:51 -0400

    Everything seems to work again

--- Chrome/src/inject/inject.js
+++ Chrome/src/inject/inject.js
@@ -1,21 +1,45 @@
-(function() {
-    function isGist() {
-       return window.location.toString().toLowerCase().indexOf("https://gist.github") > -1;
-    }
-    function createConfirmationMessage() {
-        var messageName = isGist() ? "ConfirmGist" : "ConfirmProject";
-        return chrome.i18n.getMessage(messageName);
-    }
-	var forkButton = document.querySelector(".fork-button");
-    function onFork(event) {
-        var result = confirm(createConfirmationMessage());
-        if(!result) {
-            event.preventDefault();
-            event.stopPropagation();
-            event.stopImmediatePropagation();
-            return false;
-        }
-        return true;
-    }
-    forkButton.addEventListener("click", onFork, false);
+(function () {
+	function GithubForkConfirmer(messageSource) {
+		var gfcAttributeName = "githubForkConfirmer";
+		var gfcAttributeValue = "true"
+		function isGist() {
+			return window.location.toString().toLowerCase().indexOf("https://gist.github") > -1;
+		}
+		function createConfirmationMessage() {
+			var messageName = isGist() ? "ConfirmGist" : "ConfirmProject";
+			return messageSource.getMessage(messageName);
+		}
+		function onFork(event) {
+			var result = confirm(createConfirmationMessage());
+			if (!result) {
+				event.preventDefault();
+				event.stopPropagation();
+				event.stopImmediatePropagation();
+				return false;
+			}
+			return true;
+		}
+		
+		function findAndSetBindings() {	
+			// When going to a Gist from the "All Gists" page, this script is never reloaded because it's using the History API or something
+			// From a content script we don't have access to history events (I couldn't find them anyway)
+			// So instead we wait for any click event on the page and then look for the fork button and subscribe to its click event
+			document.addEventListener("click", function(event) {
+				var selector = ".fork-button";
+				var forkButton = document.querySelector(selector);
+				if(forkButton) {
+					var hasAttribute = forkButton.getAttribute(gfcAttributeName);
+					if(!hasAttribute) {
+						forkButton.setAttribute(gfcAttributeName, gfcAttributeValue);
+						forkButton.addEventListener("click", onFork);
+					}
+				}
+			}, true);
+		}
+		this.bind = function() {
+			findAndSetBindings();	
+		}
+	}
+	var confirmer = new GithubForkConfirmer(chrome.i18n);
+	confirmer.bind();
 })();
--- UserScript/GithubForkConfirmation.user.js
+++ UserScript/GithubForkConfirmation.user.js
@@ -4,24 +4,70 @@
 // @version    1.0
 // @description  Adds a Confirm dialog to Github's Fork button 
 // @match      https://github.com/*
-// @match      https://gist.github.com/*
+// @match      https://gist.github.com/* 
+// @include      https://github.com/*
+// @include      https://gist.github.com/*
 // @copyright  2014+, Alex Pendleton
 // ==/UserScript==
 
-(function($) {
-    function isGist() {
-       return window.location.toString().toLowerCase().indexOf("https://gist.github") > -1;
-    }
-    function createConfirmationMessage() {
-        var forkableType = isGist() ? "Gist" : "project";
-        // TODO: Multi-language support?
-        return "Are you positive you want to fork this " + forkableType + "?";        
-    }
-    $(".fork-button").click(function(event) {
-        var result = confirm(createConfirmationMessage());
-        if(!result) { 
-            event.stopImmediatePropagation(); 
-            return false;
-        }
-    });
-})(unsafeWindow.jQuery);
+(function () {
+	function GithubForkConfirmer(messageSource) {
+		var gfcAttributeName = "githubForkConfirmer";
+		var gfcAttributeValue = "true"
+		function isGist() {
+			return window.location.toString().toLowerCase().indexOf("https://gist.github") > -1;
+		}
+		function createConfirmationMessage() {
+			var messageName = isGist() ? "ConfirmGist" : "ConfirmProject";
+			return messageSource.getMessage(messageName);
+		}
+		function onFork(event) {
+			var result = confirm(createConfirmationMessage());
+			if (!result) {
+				event.preventDefault();
+				event.stopPropagation();
+				event.stopImmediatePropagation();
+				return false;
+			}
+			return true;
+		}
+		
+		function findAndSetBindings() {	
+			// When going to a Gist from the "All Gists" page, this script is never reloaded because it's using the History API or something
+			// From a content script we don't have access to history events (I couldn't find them anyway)
+			// So instead we wait for any click event on the page and then look for the fork button and subscribe to its click event
+			document.addEventListener("click", function(event) {
+				var selector = ".fork-button";
+				var forkButton = document.querySelector(selector);
+				if(forkButton) {
+					var hasAttribute = forkButton.getAttribute(gfcAttributeName);
+					if(!hasAttribute) {
+						forkButton.setAttribute(gfcAttributeName, gfcAttributeValue);
+						forkButton.addEventListener("click", onFork);
+					}
+				}
+			}, true);
+		}
+		this.bind = function() {
+			findAndSetBindings();	
+		}
+	}
+    function MessageProvider() {
+		this.getMessage = function(name) {
+			var map = {
+			  "ConfirmProject": {
+				"message":"Are you positive you want to fork this project?"
+				,"description":"Confirmation text for forking a project."
+			  }
+			  ,"ConfirmGist": {
+				"message":"Are you positive you want to fork this Gist?"
+				,"description":"Confirmation text for forking a Gist."
+			  }
+			};
+			var message = map[name];
+            return message ? message.message : "";
+		};
+	}
+	var confirmer = new GithubForkConfirmer(new MessageProvider());
+	confirmer.bind();
+})();
